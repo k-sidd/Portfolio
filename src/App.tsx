8b315911392048b6b06cc03328554b31ecb549bd
Siddhant <siddhantkamble022004@gmai.com>
2024-10-27 11:11:46 +0530

    Name revamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,103 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Technologies } from './components/Technologies'
-import { TopBar } from './components/TopBar'
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+import { TechnologiesV2 } from './components/Technologies';
+import { TopBar } from './components/TopBar';
+import Typed from 'typed.js';
 
 function App() {
-    const [showElement, setShowElement] = useState(false);
+    const [imageLoaded, setImageLoaded] = useState(false);
+    const [showElement, setShowElement] = useState(false)
+
+    const backgroundUrl2 = 'https://images.pexels.com/photos/29112651/pexels-photo-29112651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = backgroundUrl2;
+        img.onload = () => setImageLoaded(true);
+    }, []);
+
+    const el = useRef(null);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (el.current) {
+                const typed = new Typed(el.current, {
+                    strings: ['Siddhant Kamble'],
+                    typeSpeed: 25,
+                    cursorChar: '',
+
+                    // showCursor: false
+                });
+
+                return () => {
+                    typed.destroy();
+                };
+            }
+        }, 0);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [imageLoaded]);
 
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowElement(true);
-        }, 1500)
+        }, 600)
 
         return () => {
             clearTimeout(timer)
         }
-    })
+    }, [imageLoaded])
 
 
-    return <div >
-        <div style={{ backgroundImage: 'url("https://i.ibb.co/x6cbMgw/sandbg.jpg)', backgroundSize: "", backgroundPosition: "center", backgroundRepeat: "repeat" }}>
-          <div className='text-[#05B2DC] bg-gradient-to-b from-[#0f172ae9] from-50% to-[#0f172abc]'>
-        <div className={`${showElement ? 'animate-fadeIn' : 'opacity-0'}`}>
-                <TopBar />
-            </div>
-        </div>
-            <div className='bg-[#0f172abc] text-[#d5f4fc] h-full pb-16 -z-10' >
-                <div className='flex justify-center w-full font-main px-12 pt-16 rounded-xl'>
-                    <div>
-                        <div className='overflow-hidden whitespace-nowrap inline-block border-r-[32px] border-lBlue text-8xl font-semibold animate-typing'>
-                            Siddhant Kamble
-                        </div>
-                        <div className={`${showElement ? 'animate-fadeIn' : 'opacity-0'}`}>
-                            <div className='flex justify-center mt-4 text-lg font-roboto font-semibold text-[#7aa0aa]'>
-                                I make functional and user-friendly websites and web applications
+    if (!imageLoaded) {
+        return <div className='bg-[#0B1427] h-screen'>
+
+        </div>; // Loading placeholder
+    }
+
+    return (
+        <div>
+            <div
+                style={{
+                    backgroundImage: `url("${backgroundUrl2}")`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                }}
+            >
+                <div className='text-[#05B2DC] bg-gradient-to-b from-[#0f172ae9] from-50% to-[#0f172abc]'>
+                    <div className={`${showElement ? 'animate-fadeIn' : 'opacity-0'}`}>
+                        <TopBar />
+                    </div>
+                </div>
+                <div className='bg-[#0f172abc] text-[#d5f4fc] h-full pb-16 -z-10'>
+                    <div className='flex justify-center w-full font-main px-12 pt-16 rounded-xl'>
+                        <div>
+                            <div className='overflow-hidden whitespace-nowrap inline-block text-8xl font-semibold'>
+                                <span ref={el} className='pr'></span>
                             </div>
-                            <div className='flex justify-center m-7 text-4xl text-[#7aa0aa] font-semibold uppercase font-roboto'>
-                                Familiar with:
+                            <div className={`${showElement ? 'animate-fadeIn' : 'opacity-0'}`}>
+                                <div className='flex justify-center mt-4 text-lg font-roboto font-semibold text-[#7aa0aa]'>
+                                    I make functional and user-friendly websites and web applications
+                                </div>
+                                <div>
+                                    {/* <span ref={el} /> */}
+                                </div>
+                                <div className='flex justify-center m-7 text-4xl text-[#7aa0aa] font-semibold uppercase font-roboto'>
+                                    Familiar with:
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className={`${showElement ? 'animate-fadeIn' : 'opacity-0'}`}>
-                    <Technologies />
+                    <div className={`${showElement ? 'animate-fadeIn' : 'opacity-0'}`}>
+                        <TechnologiesV2 />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,26 +1,20 @@
-import { firstFour, secondFour } from "../Technologies"
+import { technologies } from "../Technologies"
 
-export const Technologies = () => {
-    return <div>
-        <FourIcons setOfFour={firstFour} />
-        <FourIcons setOfFour={secondFour} />
-    </div >
-}
-
-function FourIcons({setOfFour}: {setOfFour: object}) {
+export const TechnologiesV2 = () => {
     return <div className="w-full">
         <div className="flex justify-center">
             <div className="grid grid-cols-4">
-                {Object.entries(setOfFour).map(([key, url]) => (
-                    <TechLogo key={key} url={url} />
+                {technologies.map((technology, index) => (
+                    <div className="flex items-center my-4 px-4">
+                        <img
+                            key={index}
+                            className="object-contain w-32 h-32 flex items-center bg-[#ffffff75] p-4 rounded-lg hover:scale-110 transition transform duration-300"
+                            src={technology.url}
+                            alt={technology.name}
+                        />
+                    </div>
                 ))}
             </div>
         </div>
-    </div>
-}
-
-function TechLogo({ url }: { url: string }) {
-    return <div className="flex items-center my-4 px-4">
-        <img className="object-contain w-32 h-32 flex items-center bg-[#ffffff75] p-4 rounded-lg hover:scale-110 transition transform duration-300" src={url} alt="" />
     </div>
 }
